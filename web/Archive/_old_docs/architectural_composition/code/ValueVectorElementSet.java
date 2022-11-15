fdf6de7e5cd103cb14ef296f7e5bbfea144afa5b
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2022-11-15 14:16:47 +0100

    remove unused/archive files

--- web/Archive/_old_docs/architectural_composition/code/ValueVectorElementSet.java
+++
@@ -1,3 +0,0 @@
-ValueVector vVector = v.getChildren("subnode1");
-Value thirdElement = vVector.get( 2 );
-thirdElement.setValue("Millennium Falcon");
--- web/Archive/_old_docs/basics/code/data_structures_7_2.ol
+++
@@ -1,3 +0,0 @@
-myArray[ #myArray ].first  = "1";
-myArray[ #myArray ].second = "2";
-myArray[ #myArray ].third  = "3"
